a6a9a4a5dc63d68383de9e539674df7ecbcb9637
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-30 15:11:51 +0300

    Update PaymentsDisplay.vue

--- src/components/PaymentsDisplay.vue
+++ src/components/PaymentsDisplay.vue
@@ -46,18 +46,6 @@
 </template>
 
 
-<!--<template>
-  <v-data-table
-    :headers="headers"
-    :items="all"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
-</template>-->
-
-
-
-
 
 <!--<template>
    <v-container>  
